532f198768d212d352469ee3056760ca32734f34
Nagendra Mahesh <namahesh@cisco.com>
2021-02-08 20:53:58 +0530

    SBT project init + basic kafka streams with simple echo pipe

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "kafka-playground"
+version := "0.1"
+
+libraryDependencies ++= Seq(
+  "com.lightbend" % "kafka-streams-scala_2.12" % "0.2.1" % "compile"
+)
---
+++ src/main/scala/xyz/nagendra/kafka/streaming/Util.scala
@@ -0,0 +1,22 @@
+package xyz.nagendra.kafka.streaming
+
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+
+import java.util.Properties
+
+object Util {
+  def kafkaStreamsProps: Properties = {
+    val props: Properties = new Properties()
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe")
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+    props
+  }
+
+  def closeStream(streams: KafkaStreams): Unit = {
+    println(s"Executing shutdown hook... closing kafka stream")
+    streams.close()
+  }
+}
